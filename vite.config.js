7927e89e6ee0163f2e40006f55fd44ac5eca164f
PKAPOOR4 <pkapoor4@partner.jaguarlandrover.com>
2026-04-14 12:39:02 +0100

    scalling set

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: process.env.VITE_BASE_URL || "/",
-//   // base: "/bento-hub-ui/",
-
-//   plugins: [react(),tailwindcss()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
