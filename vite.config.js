d3d0408eb92a58f71c6f3865ae4444907f000960
PKAPOOR4 <pkapoor4@partner.jaguarlandrover.com>
2026-04-14 12:19:09 +0100

    done

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: process.env.VITE_BASE_URL || "/",
+//   // base: "/bento-hub-ui/",
+
+//   plugins: [react(),tailwindcss()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  // base: process.env.VITE_BASE_URL || "/",
-  base: "/bento-hub-ui/",
-
-  plugins: [react(),tailwindcss()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/bento-hub-ui/" : "/",
+    plugins: [react(), tailwindcss()],
+  }
 })
