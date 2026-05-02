907f789722042527ba0cec17b5455742c42d6869
PKAPOOR4 <pkapoor4@partner.jaguarlandrover.com>
2026-05-02 04:43:49 +0100

    done

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,35 @@
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
-  import tailwindcss from '@tailwindcss/vite'
+  // import { defineConfig } from 'vite'
+  // import react from '@vitejs/plugin-react'
+  // import tailwindcss from '@tailwindcss/vite'
 
-  export default defineConfig(({ mode }) => {
-    return {
-      base: mode === "production" ? "/bento-hub-ui/" : "/",
-      plugins: [react(), tailwindcss()]
+  // export default defineConfig(({ mode }) => {
+  //   return {
+  //     base: mode === "production" ? "/bento-hub-ui/" : "/",
+  //     plugins: [react(), tailwindcss()]
+  //   }
+  // })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/bento-hub-ui/" : "/",
+    plugins: [react(), tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ['react', 'react-dom'],
+            ui: ['framer-motion', 'lucide-react'],
+            utils: ['lodash', 'axios']
+          }
+        }
+      },
+      // chunkSizeWarningLimit: 1000 // optional, raises the warning threshold
     }
-  })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig(({ mode }) => {
-//   return {
-//     base: mode === "production" ? "/bento-hub-ui/" : "/",
-//     plugins: [react(), tailwindcss()],
-//     build: {
-//       rollupOptions: {
-//         output: {
-//           manualChunks: {
-//             react: ['react', 'react-dom'],
-//             ui: ['framer-motion', 'lucide-react'],
-//             utils: ['lodash', 'axios']
-//           }
-//         }
-//       },
-//       // chunkSizeWarningLimit: 1000 // optional, raises the warning threshold
-//     }
-//   }
-// })
+  }
+})
 
